a90c6e2a46e30f755b6f8c3281ad3f0cb6054e60
JuanFranCM <jfchamac@gmail.com>
2026-03-06 13:42:34 +0100

    Login restriction and modal fixes

--- app/front/frontend/src/app/app.routes.ts
+++ app/front/frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { authGuard } from './guards/auth.guard';
+import { noAuthGuard } from './guards/no-auth.guard';
 import { LandingComponent } from './components/landing/landing.component';
 import { ColectionComponent } from './components/colection/colection.component';
 import { OutfitComponent } from './components/outfit/outfit.component';
@@ -10,8 +11,8 @@ import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
   // Rutas públicas
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent, canActivate: [noAuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [noAuthGuard] },
 
   // Ruta protegida
   { 
